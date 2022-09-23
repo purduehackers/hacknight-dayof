090d896fe90c3e550c1418ab8e941885c365027e
chi-wei-lien <lien2@purdue.edu>
2022-09-23 18:49:34 -0400

    add galaxy bg

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -15,15 +15,18 @@ const Header = () => {
   }, []);
 
   return(
-    <div className="h-screen no-scroll-bar bg-amber-100 flex justify-center items-center text-center">
-      <img src="/ph-logo.png" className="w-80"></img>
-      <div className="font-bold text-9xl text-slate-800">
-        <span className="underline decoration-amber-400">Hack </span>
-        <span className="underline decoration-violet-400">Night </span>
-        <span className="text-pink-400">0.2</span><br />
-        <span className="text-6xl text-slate-600">{date.toLocaleTimeString()}</span>
+    <div className="h-screen no-scroll-bar bg-[url('/galaxy.jpg')] bg-no-repeat	bg-center bg-cover flex justify-center items-center">
+      <div className="flex justify-center items-center text-center bg-amber-100 p-10 shadow-blocks rounded-lg">
+        <img src="/ph-logo.png" className="w-80"></img>
+        <div className="font-bold text-9xl text-slate-800">
+          <span className="underline decoration-amber-400">Hack </span>
+          <span className="underline decoration-violet-400">Night </span>
+          <span className="text-pink-400">0.2</span><br />
+          <span className="text-5xl text-slate-500">{date.toLocaleTimeString()}</span>
+        </div>
       </div>
     </div>
+    
   )
 }
 

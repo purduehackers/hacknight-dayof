c37414d71a405e590d10c4631d46935326553de0
chi-wei-lien <lien2@purdue.edu>
2022-09-23 17:55:33 -0400

    add timer

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,12 +1,27 @@
+import {useState, useEffect} from 'react';
+
 const Header = () => {
+  const [date, setDate] = useState(new Date());
+
+  function fetchTime() {
+    setDate(new Date());
+  }
+
+  useEffect(() => {
+    const timeId = setInterval(fetchTime, 1000); // call fetchTime every one sec
+    return function cleanup() {
+      clearInterval(timeId);
+    };
+  }, []);
+
   return(
     <div className="h-screen no-scroll-bar bg-amber-100 flex justify-center items-center text-center">
       <img src="/ph-logo.png" className="w-80"></img>
       <div className="font-bold text-9xl text-slate-800">
         <span className="underline decoration-amber-400">Hack </span>
         <span className="underline decoration-violet-400">Night </span>
-        <span>0.2</span><br />
-        <span className="text-6xl text-slate-600">9:00 PM </span>
+        <span className="text-pink-400">0.2</span><br />
+        <span className="text-6xl text-slate-600">{date.toLocaleTimeString()}</span>
       </div>
     </div>
   )
